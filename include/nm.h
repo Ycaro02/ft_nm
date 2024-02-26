c667b81fcb9554e6f25bad0972f4e9cfc9c2b374
Ycaro02 <ycaro@Kali.home>
2024-02-26 15:55:00 -0600

    clean elf header identification macro

--- include/nm.h
+++ include/nm.h
@@ -11,8 +11,9 @@
 typedef struct stat t_stat;
 
 /* Get ELF header field */
-# define ELF_HFIELD(ptr, field)  (((Elf64_Ehdr *) ptr)->e_ident[field])
-
+# define ELF_HFIELD(ptr, field)		(((Elf64_Ehdr *) ptr)->e_ident[field])
+/* Alias to easy call ELF Hfield ptr, class*/
+# define ELF_CLASS(ptr)				ELF_HFIELD(ptr, EI_CLASS)
 
 /* parse elf_header */
 void *parse_elf_header(char *str);
