fdb89882b2f04cb6f29237ee724fe7c42c76fc0e
Ycaro02 <ycaro02@hotmailfr>
2024-02-26 19:54:57 +0100

    continue elf definition test structure

--- src/main.c
+++ src/main.c
@@ -4,7 +4,7 @@
 #include <sys/stat.h>		/* fstat */
 #include <stdlib.h>			/* exit NULL */
 #include <stdio.h>			/* PERROR */
-
+# include "../libft/basic_define.h"
 #include "../libft/libft.h" 	/* libft */
 
 typedef struct stat t_stat;
@@ -20,6 +20,27 @@ int call_open(char *str)
 	return (fd);
 }
 
+
+int check_identification_byte(void *elf_struct, int byte, int value)
+{
+	int ret = ((Elf64_Ehdr *) elf_struct)->e_ident[byte] == value;
+	if (!ret) {
+		ft_printf_fd(2, "Index %d, not a valid value %d, expected %d\n", byte, (((Elf64_Ehdr *) elf_struct)->e_ident[byte]), value);
+	}	
+	return (ret);
+}
+
+int header_identification_correct(void *elf_struct)
+{
+	if (!check_identification_byte(elf_struct, EI_MAG0, ELFMAG0)\
+		|| !check_identification_byte(elf_struct, EI_MAG1, ELFMAG1)\
+		|| !check_identification_byte(elf_struct, EI_MAG2, ELFMAG2)\
+		|| !check_identification_byte(elf_struct, EI_MAG3, ELFMAG3)) {
+		return (FALSE);
+	}
+	// ft_strncmp(((Elf64_Ehdr *) elf_struct)->e_ident, ELFMAG, SELFMAG);
+	return (TRUE);
+}
 
 int main(int argc, char **argv)
 {
@@ -38,6 +59,10 @@ int main(int argc, char **argv)
 		void *elf_struct = mmap(NULL, len, PROT_READ, MAP_PRIVATE, fd, 0);
 		if (elf_struct != MAP_FAILED) {
 			ft_printf_fd(1, "Mmap success\n");
+			for (int i = 0; i < 16; ++i) {
+				ft_printf_fd(1, "[%d]: |%d|\n", i, ((Elf64_Ehdr *)elf_struct)->e_ident[i]);
+			}
+			header_identification_correct(elf_struct);
 			munmap(elf_struct, len);
 		} else {
 			perror("Error fstat:\'"); 
