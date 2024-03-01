7f398c9e10470bf85d4d91eee1759a4e3007f62a
Ycaro02 <ycaro02@hotmailfr>
2024-03-01 19:24:36 +0100

    push endian reverse test

--- include/nm.h
+++ include/nm.h
@@ -3,21 +3,33 @@
 #include <fcntl.h>			/* open */
 #include <sys/stat.h>		/* fstat */
 #include <stdlib.h>			/* exit NULL */
-#include <stdio.h>			/* PERROR */
-# include "../libft/basic_define.h"
+#include <stdio.h>			/* Perror */
+# include "../libft/basic_define.h" /* Color */
 #include "../libft/libft.h" 	/* libft */
 
 /* struct stat typedef */
 typedef struct stat t_stat;
 
+/* Start macro parse header ident */
 /* Get ELF header field */
 # define ELF_HFIELD(ptr, field)		(((Elf64_Ehdr *) ptr)->e_ident[field])
 /* Alias to easy call ELF Hfield ptr, class*/
 # define ELF_CLASS(ptr)				ELF_HFIELD(ptr, EI_CLASS)
 /* is 64 bits format */
 # define IS_ELF64(ptr)				ELF_CLASS(ptr) == ELFCLASS64
+/* Get type */
 /* get type by class */
 # define ELF_TYPE(ptr)				IS_ELF64(ptr) ? ((Elf64_Ehdr *) ptr)->e_type : ((Elf32_Ehdr *) ptr)->e_type  
+/* Get machine */
+# define ELF_MACHINE(ptr)           IS_ELF64(ptr) ? ((Elf64_Ehdr *) ptr)->e_machine : ((Elf32_Ehdr *) ptr)->e_machine 
+/* Get version */
+# define ELF_VERSION(ptr)           IS_ELF64(ptr) ? ((Elf64_Ehdr *) ptr)->e_version : ((Elf32_Ehdr *) ptr)->e_version 
+
+typedef struct s_elf {
+    char    *name;
+    uint8_t class;
+    uint8_t endian;
+} t_elf;
 
 
 /* parse elf_header */
--- src/main.c
+++ src/main.c
@@ -1,5 +1,35 @@
 # include "../include/nm.h"
 
+void    reverse_bytes(uint8_t *ptr, size_t max)
+{
+    uint8_t    tmp;
+
+    for (size_t i = 0; i < max / 2; ++i) {
+        tmp = ptr[i];
+        ptr[i] = ptr[max - i - 1];
+        ptr[max - i - 1] = tmp;
+    }
+}
+
+void test_reverse_byte()
+{
+	int nb = 9;
+	unsigned char *str = ft_calloc(sizeof(unsigned char*), nb);
+	for (int i = 0; i < nb; ++i) {
+		str[i] = i;
+	}
+	ft_printf_fd(1, YELLOW"Before:\n"RESET);
+	for (int i = 0; i < nb; ++i) {
+		ft_printf_fd(1, GREEN"%d:\n"RESET, str[i]);
+	}
+
+	reverse_bytes(str, nb);
+
+	ft_printf_fd(1, YELLOW"After:\n"RESET);
+	for (int i = 0; i < nb; ++i) {
+		ft_printf_fd(1, BLUE"%d:\n"RESET, str[i]);
+	}
+}
 
 int main(int argc, char **argv)
 {
@@ -13,5 +43,8 @@ int main(int argc, char **argv)
 	}
 	ft_printf_fd(1, "class: %d\n", ELF_CLASS(elf_struct));
 	ft_printf_fd(1, "type: %d == %d\n", ELF_TYPE(elf_struct), ((Elf64_Ehdr *) elf_struct)->e_type);
+	ft_printf_fd(1, "machine: %d == %d\n", ELF_MACHINE(elf_struct), ((Elf64_Ehdr *) elf_struct)->e_machine);
+	ft_printf_fd(1, "version: %d == %d\n", ELF_VERSION(elf_struct), ((Elf64_Ehdr *) elf_struct)->e_version);
+	test_reverse_byte();
 	return (0);
 }
