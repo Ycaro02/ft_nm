06fe399ce8512ca1af6faba0066a97ac656343cb
Ycaro02 <ycaro@Kali.home>
2024-03-04 13:42:15 -0600

    add exit code tester for real nm

--- rsc/diff.sh
+++ rsc/diff.sh
@@ -24,5 +24,36 @@ elf32_basic_test() {
     rm ${NAME32}
 }
 
+test_exit_code() {
+    nm ${1} > nm_out 2> /dev/null; echo -e "${YELLOW}${1}\tExit code:${RESET} ${GREEN}${?}${RESET}"
+    rm nm_out
+}
+
+test_perm_file() {
+    PERM=$1
+    OLD=$2
+    if [ ! ${PERM} -eq ${OLD} ];
+    then
+        mv perm_${OLD} perm_${1}
+    else
+        touch perm_${1}
+    fi
+    chmod ${1} perm_${1}
+    test_exit_code perm_${1}
+    chmod 777 perm_${1}
+}
+
+exit_code_tester() {
+    test_exit_code ${BIN}
+    test_exit_code " "
+    test_exit_code dsa
+    test_perm_file 000 000
+    test_perm_file 666 000
+    test_perm_file 444 666
+    test_perm_file 111 444
+    rm perm_111
+}
+
 elf_file_diff ${BIN}
 elf32_basic_test rsc/main_32.c
+exit_code_tester
