782cff5edfbee5116d2d2eb3e57b36cec3a95fcb
Ycaro02 <ycaro@Kali.home>
2024-03-10 11:08:57 -0500

    try tester

--- rsc/call_tester.sh
+++ rsc/call_tester.sh
@@ -3,35 +3,36 @@
 TESTER="rsc/nm_tester.sh" 
 NAME="ft_nm"
 
+VALGRIND_OPT=0
+
+if [ ! -z $1 ]; then
+VALGRIND_OPT=$1
+fi
+
 ./rsc/mk/ascii.sh "tester"
 
 
 # ${TESTER} ${NAME} 1
 
-# ${TESTER} ${NAME} 0 "-p" "-p"
-# ${TESTER} ${NAME} 1 "--no-sort" "-p"
+${TESTER} ${NAME} 0 "-p" "-p"
 
-# ${TESTER} ${NAME} 0 "-r" "-r"
-# ${TESTER} ${NAME} 1 "-r" "-r"
+# ${TESTER} ${NAME} ${VALGRIND_OPT} "-r" "-r"
 
-# ${TESTER} ${NAME} 0 "-u" "-u"
-# ${TESTER} ${NAME} 1 "-u" "-u"
+# ${TESTER} ${NAME} ${VALGRIND_OPT} "-u" "-u"
 
-# ${TESTER} ${NAME} 0 "-g" "-g"
-# ${TESTER} ${NAME} 1 "-g" "-g"
+# ${TESTER} ${NAME} ${VALGRIND_OPT} "-g" "-g"
 
-# ${TESTER} ${NAME} 0 "-a" "-a"
-${TESTER} ${NAME} 1 "-a" "-a"
+# ${TESTER} ${NAME} ${VALGRIND_OPT} "-a" "-a"
 
 
-# ${TESTER} ${NAME} 1 "-rp" "-rp"
-# ${TESTER} ${NAME} 1 "-ru" "-ru"
-# ${TESTER} ${NAME} 1 "-pu" "-pu"
+# ${TESTER} ${NAME} ${VALGRIND_OPT} "-rp" "-rp"
+# ${TESTER} ${NAME} ${VALGRIND_OPT} "-ru" "-ru"
+# ${TESTER} ${NAME} ${VALGRIND_OPT} "-pu" "-pu"
 
-# ${TESTER} ${NAME} 1 "-rpu" "-rpu"
+# ${TESTER} ${NAME} ${VALGRIND_OPT} "-rpu" "-rpu"
 
-# ${TESTER} ${NAME} 1 "-gu" "-gu"
-# ${TESTER} ${NAME} 1 "-gp" "-gp"
-# ${TESTER} ${NAME} 1 "-gr" "-gr"
+# ${TESTER} ${NAME} ${VALGRIND_OPT} "-gu" "-gu"
+# ${TESTER} ${NAME} ${VALGRIND_OPT} "-gp" "-gp"
+# ${TESTER} ${NAME} ${VALGRIND_OPT} "-gr" "-gr"
 
-# ${TESTER} ${NAME} 1 "-grpu" "-grpu"
+# ${TESTER} ${NAME} ${VALGRIND_OPT} "-grpu" "-grpu"
